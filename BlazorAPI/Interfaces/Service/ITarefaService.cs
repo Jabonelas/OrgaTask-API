26b58f6ae1a78bca308a0423c4347cc384fd1bfa
Lailton Oliveira dos Santos <infobase.lsantos@terceiros.mag.com.br>
2025-05-07 11:59:52 -0300

    Alterações na classe de serviço onde métodos privados podem ser static e nem todo método Task precisa ter async await se ele s'ovai chamar ou fazer o retorno de um código.

--- BlazorAPI/Interfaces/Service/ITarefaService.cs
+++ BlazorAPI/Interfaces/Service/ITarefaService.cs
@@ -1,23 +1,19 @@
 ﻿using BlazorAPI.DTOs;
 using BlazorAPI.DTOs.Tarefa;
-using BlazorAPI.Models;
-using Microsoft.AspNetCore.Mvc;
-using static BlazorAPI.Controllers.TarefaController;
 
-namespace BlazorAPI.Interfaces.Service
+namespace BlazorAPI.Interfaces.Service;
+
+public interface ITarefaService
 {
-    public interface ITarefaService
-    {
-        Task CadastrarTarefaAsync(int _idUsuario, TarefaCadastrarDTO _dadosTarefaCadastro);
+    Task CadastrarTarefaAsync(int idUsuario, TarefaCadastrarDTO dadosTarefaCadastro);
 
-        Task AlterarTarefaAsync(TarefaCadastrarDTO _dadosTarefaCadastro, int _idUsuario);
+    Task AlterarTarefaAsync(TarefaCadastrarDTO dadosTarefaCadastro, int idUsuario);
 
-        Task DeletarTarefaAsync(int _idTarefa, int _idUsuario);
+    Task DeletarTarefaAsync(int _idTarefa, int _idUsuario);
 
-        Task<List<TarefaConsultaDTO>> ListaTarefasIdAsync(int _idUsuario);
+    Task<List<TarefaConsultaDTO>> ListaTarefasIdAsync(int _idUsuario);
 
-        Task<PagedResult<TarefaConsultaDTO>> ListaTarefasPaginadasAsync(int _idUsuario, int _pageNumber, int _pageSize);
+    Task<PagedResult<TarefaConsultaDTO>> ListaTarefasPaginadasAsync(int _idUsuario, int _pageNumber, int _pageSize);
 
-        Task<TarefaCadastrarDTO> BuscarTarefaAsync(int _idTarefa, int _idUsuario);
-    }
+    Task<TarefaCadastrarDTO> BuscarTarefaAsync(int _idTarefa, int _idUsuario);
 }
