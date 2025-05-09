37d1bb1fc8ca682393971f6ba2bc8f49484d976c
israel <israel094@hotmail.com>
2025-05-08 21:39:13 -0300

    correcao render

--- BlazorAPI/Controllers/TarefaController.cs
+++ BlazorAPI/Controllers/TarefaController.cs
@@ -295,9 +295,21 @@ public class TarefaController : ControllerBase
         {
             return NotFound(new ErrorResponse { message = ex.Message });
         }
-        catch (Exception)
+        //catch (Exception)
+        //{
+        //    return StatusCode(500, new ErrorResponse { message = "Erro interno ao buscar lista tarefas." });
+        //}
+        catch (Exception ex)
         {
-            return StatusCode(500, new ErrorResponse { message = "Erro interno ao buscar lista tarefas." });
+            //_logger.LogError(ex, "Erro completo: " + ex.ToString());
+
+            return StatusCode(500, new
+            {
+                Message = "Erro interno",
+                Detalhes = ex.Message,
+                StackTrace = ex.StackTrace,
+                InnerException = ex.InnerException?.Message
+            });
         }
     }
 
